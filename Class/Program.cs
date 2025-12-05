058aa38e2416414ab7963bb8535be09818267929
Ancho17 <quins1819@gmail.com>
2025-12-05 18:23:54 +0000

    test 2

---
+++ Class/Program.cs
@@ -0,0 +1,95 @@
+﻿namespace Class
+{
+    class MyClass
+    {
+        static void Main()
+        {
+            Cat Doris = new("Doris", 3);
+            System.Console.WriteLine($"The name is {Doris.Name} it is {Doris.Age} and it has {Doris.Lives} lives.");
+            Doris.MakeSound();
+            Doris.Love();
+            
+
+            Dog Bary = new("Bary",4);
+            System.Console.WriteLine($"The name is {Bary.Name} and is {Bary.Age} years old it's tail is {Bary.Tail}.");
+            Bary.MakeSound();
+            Bary.Love();
+          
+
+        }
+
+
+    }
+  
+    abstract public class Pet
+    {
+        
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public Pet(string name, int age) =>
+        (Name, Age) = (name, age);
+        virtual public void MakeSound()
+        {
+            System.Console.WriteLine("The animal is saying: ");
+            Sound();
+        
+        }
+        abstract public void Sound();
+        
+
+
+
+    }
+    interface ILove
+    {
+     void Love();
+        
+    }
+
+    public class Cat : Pet,ILove
+    {
+        public Cat(string name, int age)
+        : base(name, age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public int Lives { get; } = 9;
+        
+        public override void Sound()
+        {
+
+            System.Console.WriteLine("*Meow*");
+        }
+        public void Love()
+        {
+            System.Console.WriteLine("I love you");
+        }
+    }
+     public class Dog : Pet,ILove
+    {
+        public Dog(string name, int age)
+        : base(name, age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Tail = "wagging";
+        
+        public override void Sound()
+        {
+
+            System.Console.WriteLine("*Wof*");
+        }
+        public void Love()
+        {
+            System.Console.WriteLine("I love you");
+        }
+    }
+    
+
+
+}
+
+
