991cc781f348a45328ef40ee2d747d88db4b9d60
Ancho17 <quins1819@gmail.com>
2025-12-05 18:18:53 +0000

    Test

---
+++ Practise/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Practise
+{
+class Base
+    {
+        static void Main()
+        {
+          
+        List<Person> people = new(3);
+        
+        for(int i=0;i<3;i++)
+            {
+            Person temp = new();
+            temp.Name=System.Console.ReadLine();
+            people.Add(temp);
+            }
+            foreach(var person in people)
+            {
+            System.Console.WriteLine(person.Name);
+            }
+
+
+        }
+    }
+     public class Person
+    {
+    public string? Name {get;set;}
+    }
+}
