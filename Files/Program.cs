7fdafe34c73d51d8f88127ec1e44f6cdda79d533
Ancho17 <quins1819@gmail.com>
2025-12-13 21:10:35 +0000

    A lot to do

---
+++ Files/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Text;
+namespace Files
+{
+class Learn_File
+    {
+    static void Main()
+        {
+        StreamWriter writer = new StreamWriter("/workspaces/C-sharp/Files/Hello.txt",false);
+        writer.Write("Hello World");
+        writer.Close();
+
+
+        }
+    }
+}
---
+++ Scounter/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Word_count
+{
+    class Begin
+    {
+    static void Main()
+        {
+            System.Console.Write("Type here: ");
+            string word = System.Console.ReadLine();
+            string word2 = word;
+            int count = 0;
+            int positon;
+            if(word=="")
+            {
+            positon = 0;
+            }
+            else
+            {
+            positon = 1;
+            }
+            int position2;
+            
+            while(positon>0)
+            {
+            positon = word.IndexOf(" ");
+            position2 = positon+1;
+            count++;
+            word=word.Substring(position2,word.Length-positon-1);
+            }
+            string[] words = word2.Split(' ');
+            
+            
+            
+            
+            System.Console.WriteLine("The number of word is "+count);
+            foreach(var s in words)
+            {
+                System.Console.WriteLine($"The lenght of {s} is {s.Length}");
+            }
+        }
+    }
+}
