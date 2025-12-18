abfd278994de1351502224cc9b5bf952d6dcb256
Ancho17 <quins1819@gmail.com>
2025-12-18 19:53:23 +0000

    Finally

--- Files/Program.cs
+++ Files/Program.cs
@@ -7,12 +7,50 @@ class Learn_File
     {
     static void Main()
         {
-       string word =System.Console.ReadLine();
-       using FileStream writter = new FileStream("/workspaces/C-sharp/Files/DesHello.txt",FileMode.Append,FileAccess.Write,FileShare.None);
-       byte[] buffer = Encoding.UTF8.GetBytes(word);
-       writter.Write(buffer,0,word.Length);
-
+       FileStream reader =new FileStream("/workspaces/C-sharp/Files/Hello.txt",FileMode.OpenOrCreate,FileAccess.Read,FileShare.Read);
+       int size =1;
+       byte[] buffer = new byte[size];
+       bool checker = false;
+       int count;
+       int countText=0;
+       int dotcount = 0;
+       string text = "";
+       string? temp ;
+       
+       while((count=reader.Read(buffer,0,size))!=0)
+            {
+                temp = Encoding.UTF8.GetString(buffer,0,size); 
+            text += temp;
+            if(temp==".")
+                {
+                    dotcount++;
+                }
+            if(checker==true&&!temp.Contains('\n'))
+                {
+                checker = false;
+                }
+                
+           
+                else{
+            if(temp.Contains(' '))
+                {
+                countText++;
+                checker=true;
+                }
+                 else if(temp.Contains('.') )
+                {
+                countText++;
+                checker=true;
+                
+                }
+                }
+                
+            }
+            System.Console.WriteLine(countText);
+            
+            
         }
+        
 
       
     }
