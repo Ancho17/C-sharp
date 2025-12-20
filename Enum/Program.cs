1a1e42e5153af4ff8543b8027c1327a133d14c73
Ancho17 <quins1819@gmail.com>
2025-12-20 18:26:58 +0000

    Enums

---
+++ Enum/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace EnumLearn
+{
+    class Enums
+    {
+    static void Main()
+        {
+     
+         List<string> mylist = ["chery","strwaberry","apple"];
+        
+         var count = Enum.GetNames(typeof(Fruit)).Length;
+         for(int i=0;i<mylist.Count;i++)
+            {
+            for(int j=0;j<count;j++)
+                {
+                Fruit mainIN = (Fruit)j;
+                string fruit = mainIN.ToString();
+                if(mylist[i]==fruit)
+                    {
+                    System.Console.WriteLine(fruit+" is in the List");
+                    }
+                }
+            }
+        }
+ 
+   
+  
+    }
+            enum Fruit
+    {
+        apple,
+        orange,
+        grape,
+        strwaberry,
+        chery
+    }
+   
+    
+}
