9c3c864dd8c8b07336ddb7588a3c0cd24bf7350e
Ancho17 <quins1819@gmail.com>
2025-12-21 11:34:46 +0000

    smth

--- File2/Program.cs
+++ File2/Program.cs
@@ -7,29 +7,8 @@ class AnotherOne
     {
     static void Main()
         {
-         using FileStream reader = new("/workspaces/C-sharp/File2/Start.txt",FileMode.OpenOrCreate,FileAccess.Read,FileShare.Read);
-         using StreamWriter writer = new("/workspaces/C-sharp/File2/Complete.inv");
-         int size=1;
-         string text = "";
+         string chars = System.Console.ReadLine();
          
-         char[] ftext;
-         byte[] buffer = new byte[size];
-         
-             
-         while(reader.Read(buffer,0,size)!=0)
-            {        
-              text +=Encoding.UTF8.GetString(buffer,0,size);
-            }
-            ftext =text.ToCharArray();
-            Array.Reverse(ftext);
-            text="";
-            foreach(var chr in ftext)
-            {
-                text+=chr;  
-            }
-           writer.Write(text);
-
-
 
 
         }
--- File2/obj/Debug/net10.0/File2.AssemblyInfo.cs
+++ File2/obj/Debug/net10.0/File2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("File2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+608c7c1dc1bcc6baf16ad3805287a1ee38b01e8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a1e42e5153af4ff8543b8027c1327a133d14c73")]
 [assembly: System.Reflection.AssemblyProductAttribute("File2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("File2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
