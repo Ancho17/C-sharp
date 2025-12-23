0094baff8204f5a688f312e7a6b1dbab33a44903
Ancho17 <quins1819@gmail.com>
2025-12-23 19:57:15 +0000

    XML

---
+++ XML/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Xml;
+namespace XMLer
+{
+class XMiLier
+    {
+    static void Main()
+        {
+       var reader = XmlReader.Create("products.xml");
+        reader.ReadToFollowing("product");
+       do{
+       reader.MoveToFirstAttribute();
+       System.Console.WriteLine(reader.Value);
+       reader.ReadToFollowing("name");
+       System.Console.WriteLine(reader.ReadElementContentAsString());
+       reader.ReadToNextSibling("price");
+       System.Console.WriteLine(reader.ReadElementContentAsString());
+       } while( reader.ReadToFollowing("product"));
+
+
+
+        }
+    }
+}
