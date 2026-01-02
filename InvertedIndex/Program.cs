031ce708a85558abe742c27778ada967553e320d
Ancho17 <quins1819@gmail.com>
2026-01-02 17:48:38 +0000

    Inverted Index

---
+++ InvertedIndex/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Inverted
+{
+class Index
+    {
+        static void Main()
+        {
+        System.Console.Write("Search for word ");
+        string? word = System.Console.ReadLine();
+        string[] tokens = word.Split(" ");
+        List<string> doc = ["Document1","Document2"];
+        for(int j=0;j<tokens.Length;j++){
+            System.Console.Write($"{tokens[j]} ->");
+        for(int i=0;i<doc.Count;i++)
+        {
+        using StreamReader reader = new("/workspaces/C-sharp/InvertedIndex/"+doc[i]+".txt");
+        string search = reader.ReadToEnd();
+        if(search.Contains(tokens[j]))
+            {
+                if(i!=0)
+                    {
+                        System.Console.Write(",");
+                    }
+            System.Console.Write(doc[i]);
+            }
+       
+        
+        
+        }
+        System.Console.WriteLine("");
+        }
+
+        }
+    }
+}
