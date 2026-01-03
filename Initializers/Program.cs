0917aade1a2503012d01be9f3adc58c21d01f08e
Ancho17 <quins1819@gmail.com>
2026-01-03 20:20:21 +0000

    Initializers

---
+++ Initializers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Initializers
+{
+class TrySomething
+    {
+    static void Main()
+        {
+        var cat = new Cat{Name="Jerald",Age=3};
+        System.Console.WriteLine(cat.Name);
+       
+        
+        }
+        public class Cat
+        {
+        public string? Name{get;set;}
+        public int Age{get;set;}
+        public Cat(){}
+        
+        
+        }
+    }
+}
