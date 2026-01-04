e3521f7561f5f505de2bc11f3714c3805cd0e3ac
Ancho17 <quins1819@gmail.com>
2026-01-04 17:39:40 +0000

    FlowerShop

---
+++ FlowerShop/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace FlowerShop
+{
+class MyShop
+    {
+    static void Main()
+        {
+         Flower Lily = new(){Name="Lily",Price = 2.30,Amount=0};
+         Flower Rose = new(){Name="Rose",Price = 3.60,Amount=0};
+         Bouquet LR = new(){
+         Flowers = [
+             new(){Name="Lily",Price = 2.30,Amount=3},
+             new(){Name="Rose",Price = 3.60,Amount=2}
+
+         ]
+         };
+         LR.PrizeEvaluation();
+
+         
+         
+        }
+    }
+    public class Flower
+    {
+    public  required string Name {get;init;}
+    public required double Price {get;set;}
+    public int Amount{get;set;}
+
+    public void Buy()
+        {
+        System.Console.Write("How much to buy ");
+        int quantity =Int32.Parse(System.Console.ReadLine());
+        Amount += quantity;
+        }
+    }
+    public class Bouquet
+    {
+public List<Flower> Flowers {get;set;} = [];
+public void PrizeEvaluation()
+        {
+        double Fprice =0.0;
+        foreach(var price in Flowers)
+            {
+                Fprice += price.Price;
+            }
+            System.Console.WriteLine($"The bouquets price is {Fprice}");
+        }
+    }
+}
--- Initializers/Program.cs
+++ Initializers/Program.cs
@@ -4,16 +4,25 @@ class TrySomething
     {
     static void Main()
         {
-        var cat = new Cat{Name="Jerald",Age=3};
+        Cat cat = new() {Name = "Jerald"};
         System.Console.WriteLine(cat.Name);
+        List<Cat> cats = [
+            new() {Name="Jhon",Age=3}
+        ];
+        System.Console.WriteLine(cats[0].Name);
+        Dictionary<int,string> numbers = new(){
+        [0]="Elephant",
+        [1]="Snake"
+        };
+        System.Console.WriteLine(numbers[0]);
        
         
         }
         public class Cat
         {
-        public string? Name{get;set;}
+        public required string? Name{get;set;}
         public int Age{get;set;}
-        public Cat(){}
+      
         
         
         }
