5628b89b0eafa8a39f591a828a12ac0e97fede83
Ancho17 <quins1819@gmail.com>
2026-01-06 18:28:27 +0000

    Happy Happy Happy

---
+++ HappyNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace HappyNumbers
+{
+class HappyHappy
+    {
+    static void Main()
+        {
+            System.Console.Write("Chose a number: ");
+           int number = Int32.Parse(System.Console.ReadLine());
+           int part1;
+           int part2;
+           int count =0;
+           while(number!=1)
+            {
+                if(count>10)
+                {
+                break;
+                }
+                part1 = number%10;
+                part2 = number/10;
+                number=part1*part1 + part2*part2;
+                count ++;
+
+
+            }
+            if(number==1)
+            System.Console.WriteLine("Happy Happy Happy");
+        }
+    }
+}
--- Shape/Program.cs
+++ Shape/Program.cs
@@ -4,15 +4,17 @@ class MaMen
 {
     static void Main()
         {
-        Square square = new(9,2);
+        Square square = new(9,0);
         square.Area();
+        Diamond diamond = new(4,6);
+        diamond.Area();
         }
 abstract class ShapeShifter
         {
-        public int S{get;set;}
-        public int A{get;set;}
-        public int B{get;set;}
-        public ShapeShifter(int a,int b)
+        public double S{get;set;}
+        public double A{get;set;}
+        public double B{get;set;}
+        public ShapeShifter(double a,double b)
             {
             
             A=a;
@@ -25,17 +27,28 @@ abstract class ShapeShifter
         }
     class Square : ShapeShifter
         {
-        public Square(int a,int b):base(a,b)
+        public Square(double a,double b):base(a,b)
             {
             
             }
             public override void Area()
             {
                 S=A*A;
-                System.Console.WriteLine($"The are of a square is {S}");
+                System.Console.WriteLine($"The area of a square is {S}");
             }
 
         
-        }    
-}
+        }  
+    class Diamond : ShapeShifter
+        {
+        public Diamond(double a,double b):base(a,b){}
+            public override void Area()
+            {
+                S=(A*B)/2;
+                System.Console.WriteLine($"The area of diamond is {S}");
+            }
+
+        }  
+        
+    }
 }
