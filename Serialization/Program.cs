9366666f2fff33538546b3355542a95757d2222f
Ancho17 <quins1819@gmail.com>
2026-01-08 20:38:01 +0000

    Serialize

---
+++ Serialization/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace Serialization
+{
+class Realisation
+    {
+    static void Main()
+        {
+        Numbers one = new(){first=12,second = 45,third = 89};
+        
+        string path ="/workspaces/C-sharp/Serialization/Info.json";
+        
+       
+       var option = new JsonSerializerOptions{ WriteIndented = true};
+       byte[] serialize =JsonSerializer.SerializeToUtf8Bytes(one,option);
+       
+       File.WriteAllBytes(path,serialize);
+       System.Console.WriteLine(serialize);
+        }
+        public class Numbers
+        {
+        public int first{get;set;}
+        public int second{get;set;}
+        
+        public int third{get;set;}
+        }
+    }
+}
