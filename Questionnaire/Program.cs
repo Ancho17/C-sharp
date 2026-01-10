6bd75bf461fed3cbf9ce41c83088162cd1f2db11
Ancho17 <quins1819@gmail.com>
2026-01-10 14:07:33 +0000

    Questionnaire

---
+++ Questionnaire/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Text.Json;
+
+namespace Questions
+{
+class Ask
+{
+ static void Main()
+        {
+        Question one = new();
+        All all = new();
+        int amount =5;
+        string path = "/workspaces/C-sharp/Questionnaire/Answers.json";
+        all.AllAnswers = new char[amount];
+        string[] ask = 
+        {"What is your gender \n a:male b:female c:other ",
+         "What is your age range \n a:<19 b:20-40 c:41+ ",
+         "How often do u exercise \n a:i don't b:once or twice c:pretty often ",
+         "Do u have health problems \n a:No b: pretty small ones c: pretty serious ones ",
+         "How much do u sleep at night \n a:around 8h b:less than 8h c:more than 8h "
+
+        };
+        for(int i=0;i<amount;i++){
+        all.AllAnswers[i]=AQuestion(ask[i],one.Answers);
+        }
+        string finish = JsonSerializer.Serialize(all);
+        File.WriteAllText(path,finish);
+       
+
+      
+        
+        }
+    public class Question
+        {
+        private char answers;
+        public char Answers{get {return answers;}
+        set
+                {
+                if(value=='a'||value=='b'||value=='c')
+                    {
+                    answers = value;
+                    
+                    }
+                    else 
+                    {
+                    answers = 'w';
+                    }
+                }
+        }
+        }
+    public class All
+        {
+        public char[] AllAnswers{get;set;}
+        }
+    public static char AQuestion(string ask,char answer)
+        {
+        while(true)
+            {
+            System.Console.Write(ask);
+            answer =System.Console.ReadLine()[0];
+            if(answer!='a'&&answer!='b'&&answer!='c')
+                {
+                System.Console.WriteLine("Your answer must be a,b or c");
+                }
+            else
+                {
+                break;
+                }
+            }
+        return answer;
+
+        }
+}
+}
