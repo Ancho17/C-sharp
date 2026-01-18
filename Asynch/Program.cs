9174560f7b1b680e45fb2ce61f6ca7316b7be0b5
Ancho17 <quins1819@gmail.com>
2026-01-18 17:18:44 +0000

    Asynchronys first step

---
+++ Asynch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+namespace Synchro
+{
+class Synchronizing
+    {
+static async Task Main(string[] args)
+{
+await Did();
+await Does();
+
+}
+public static async Task Does()
+        {
+        
+        string result = await File.ReadAllTextAsync("/workspaces/C-sharp/Asynch/example.txt");
+        System.Console.WriteLine(result);
+        }
+        
+        public static async Task Did()
+        {
+        await Task.Delay(2000);
+        string finish = await File.ReadAllTextAsync("/workspaces/C-sharp/Asynch/exapmle2.txt");
+        System.Console.WriteLine(finish);
+        
+        }
+
+    }
+
+}
